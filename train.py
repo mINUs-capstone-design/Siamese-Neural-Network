bb096f4f0d41bc3a7cba3647f060009eea78a350
JEON JUNPYO <76035724+NsnyaGit@users.noreply.github.com>
2024-04-14 19:51:58 +0900

    Create train.py

---
+++ train.py
@@ -0,0 +1,47 @@
+%matplotlib inline
+import torchvision
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader,Dataset
+import matplotlib.pyplot as plt
+import torchvision.utils
+import numpy as np
+import random
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import PIL.ImageOps    
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+
+train_dataloader = DataLoader(siamese_dataset,
+                        shuffle=True,
+                        num_workers=8,
+                        batch_size=Config.train_batch_size)
+                        
+                        
+net = SiameseNetwork().cuda()
+criterion = ContrastiveLoss()
+optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
+
+counter = []
+loss_history = [] 
+iteration_number= 0
+
+for epoch in range(0,Config.train_number_epochs):
+    for i, data in enumerate(train_dataloader,0):
+        img0, img1 , label = data
+        img0, img1 , label = img0.cuda(), img1.cuda() , label.cuda()
+        optimizer.zero_grad()
+        output1,output2 = net(img0,img1)
+        loss_contrastive = criterion(output1,output2,label)
+        loss_contrastive.backward()
+        optimizer.step()
+        if i %10 == 0 :
+            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
+            iteration_number +=10
+            counter.append(iteration_number)
+            loss_history.append(loss_contrastive.item())
+show_plot(counter,loss_history)
