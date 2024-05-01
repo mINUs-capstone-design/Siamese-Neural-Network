6ddf38075e8128c042a31de3148fdcedf471a7a1
JEON JUNPYO <76035724+NsnyaGit@users.noreply.github.com>
2024-05-01 15:16:16 +0900

    Update audio-to-mel-spectrogram.py
    가독성 향상을 위해 argument 및 변수의 이름 변경

--- audio-to-mel-spectrogram.py
+++ audio-to-mel-spectrogram.py
@@ -21,11 +21,11 @@ from scipy.io.wavfile import read
 
 # ----------------------------------------------------------------
 # .pcm -> .wav 변환함수
-def pcm_to_wav(pcm_file, wav_file, channels, sample_width, frame_rate):
+def pcm_to_wav(pcm_data, wav_data, channels, sample_width, frame_rate):
     # PCM 파일 열기
-    with open(pcm_file, 'rb') as pcm:
+    with open(pcm_data, 'rb') as pcm:
         # WAV 파일 열기
-        with wave.open(wav_file, 'wb') as wav:
+        with wave.open(wav_data, 'wb') as wav:
             # WAV 파일 헤더 설정
             wav.setnchannels(channels)
             wav.setsampwidth(sample_width)
@@ -38,38 +38,38 @@ def pcm_to_wav(pcm_file, wav_file, channels, sample_width, frame_rate):
 
 # ----------------------------------------------------------------
 # VAD 알고리즘
-def wav_to_vad(no_vad_wav, apply_vad_wav):
-    y, sr = librosa.load(no_vad_wav)
+def wav_to_vad(wav_data, path):
+    y, sr = librosa.load(wav_data)
     y_trimmed, index = librosa.effects.trim(y, top_db=30)
-    sf.write(apply_vad_wav, y_trimmed, sr)
+    sf.write(path, y_trimmed, sr)
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
 # 현재 위치한 디렉토리 내의 확장자가 .pcm인 모든 파일 불러오기
-pcm_directory = "/content/drive/MyDrive/kr"
+data_dir = "/content/drive/MyDrive/kr"
+save_dir = "/content/test"
 
-my_pcm_data = [f for f in os.listdir(pcm_directory) if f.endswith(".pcm")]
+original_files = [f for f in os.listdir(data_dir) if f.endswith(".pcm")]
 
 # 반복하여 한번에 여러 파일 처리
-for pcm_file in my_pcm_data:
-    # .pcm 파일의 경로
-    pcm_sound = os.path.join(pcm_directory, pcm_file)
-    
+for original_file in original_files:
+    # original 파일의 이름
+    original_path = os.path.join(data_dir, original_file)
     # .wav 파일의 경로
-    wav_file = os.path.join(pcm_directory, os.path.splitext(pcm_file)[0] + ".wav")
+    wav_path = os.path.join(save_dir, os.path.splitext(original_file)[0] + ".wav")
     
     # PCM을 WAV로 변환
-    pcm_to_wav(pcm_sound, wav_file, channels=1, sample_width=2, frame_rate=16000)
+    pcm_to_wav(original_path, wav_path, channels=1, sample_width=2, frame_rate=16000)
     print(".pcm을 .wav로 변환 완료")
     
     # VAD 적용하여 새로운 .wav 파일 저장
-    vad_wav_file = os.path.join(pcm_directory, f"VAD_{os.path.splitext(pcm_file)[0]}.wav")
-    wav_to_vad(wav_file, vad_wav_file)
-    wav_file = vad_wav_file
+    vad_wav_path = os.path.join(save_dir, f"VAD_{os.path.splitext(original_file)[0]}.wav")
+    wav_to_vad(wav_path, vad_wav_path)
+    wav_path = vad_wav_path
     print(".wav 파일에 VAD 알고리즘 적용 완료")
-    
+
     # WAV 파일 읽기
-    y, sr = librosa.load(wav_file)
+    y, sr = librosa.load(wav_path)
     
     # Mel-spectrogram 계산
     mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
@@ -98,7 +98,7 @@ for pcm_file in my_pcm_data:
     plt.colorbar().remove()
 
     # 이미지 저장 (그래프 내용 부분만...흰 배경X)
-    output_filename = os.path.join(pcm_directory, f"Mel_spectrum_{os.path.splitext(os.path.basename(wav_file))[0]}.jpg")
+    output_filename = os.path.join(save_dir, f"Mel_spectrum_{os.path.splitext(os.path.basename(wav_path))[0]}.jpg")
     plt.axis('off')
     plt.savefig(output_filename, bbox_inches='tight', pad_inches=0)
     plt.close()
