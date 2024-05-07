e963e6f07d7fd66b86498d29fc7cf9f5bfec1eb2
JEON JUNPYO <76035724+NsnyaGit@users.noreply.github.com>
2024-05-07 13:20:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -96,6 +96,49 @@ class SiameseNetworkDataset(Dataset):
         return len(self.imageFolderDataset.imgs)
 
 
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1), # 가장자리의 특징들까지 고려
+            nn.Conv2d(3, 4, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(4),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(8, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+
+
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(8*99*250, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 5))
+
+    def forward_once(self, x):
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1) # flatten
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = torch.load("siamese_net_v4.pt", map_location=device)
 print(model)
